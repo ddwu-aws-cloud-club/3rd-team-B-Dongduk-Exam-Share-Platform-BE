8f0bd6ce067d1ce435c776de9f5203a91afd5893
gaeunee2 <gaeunee2>
2026-01-16 23:21:37 +0900

    chore: configure RDS PostgreSQL and Spring datasource connection

--- src/main/java/com/somshare/somshare/controller/PostController.java
+++ src/main/java/com/somshare/somshare/controller/PostController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import java.security.Principal;
 
 @Slf4j
 @RestController
